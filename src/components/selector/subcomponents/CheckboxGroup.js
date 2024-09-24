f5309acafdba365acc506f12c9ead43aaa0b8e3a
Carolina <carolina.obreque@alumnos.uach.cl>
2024-09-24 17:50:53 -0300

    los componentes

---
+++ src/components/selector/subcomponents/CheckboxGroup.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { CheckBox, Text } from '@ui-kitten/components';
+import { View, StyleSheet } from 'react-native';
+
+export default CheckboxGroup = ({ items, onSelect, value, maxChecked }) => {
+  maxChecked ??= items.length // Si no se especifica menos, se pueden pulsar todas
+
+  const [selectedValues] = useState(new Set()) // Usar el estado como referencia de un Set
+  if (value) [value].flatMap(value => selectedValues.add(value)) // Setear los valores iniciales
+
+  const handleSelect = itemValue => {
+    if (selectedValues.has(itemValue)) selectedValues.delete(itemValue) // Eliminar si ya estaba seleccionado
+    else if (selectedValues.size < maxChecked) selectedValues.add(itemValue) // Añadir si no está seleccionado y no excede el límite
+    if (onSelect) onSelect(Array.from(selectedValues)) // Llamar al callback con los valores seleccionados
+  }
+
+  return (
+    <View style={styles.container}>
+      {items.map(item => (
+        <View key={item.value} style={styles.checkboxContainer}>
+          <CheckBox
+            checked={selectedValues.has(item.value)} // Verificar si el valor está seleccionado
+            onChange={() => handleSelect(item.value)}
+            style={styles.checkbox} // Estilo personalizado
+          >
+            <Text style={styles.checkboxText}>{item.name}</Text> {/* Texto más grande */}
+          </CheckBox>
+        </View>
+      ))}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 10
+  },
+  checkboxContainer: {
+    marginVertical: 5 // Espacio entre los checkboxes
+  },
+  checkboxText: {
+    fontSize: 18, // Tamaño de fuente más grande
+  }
+})
+
