4e83e281ddf07f6e58b6973ed22c145817680cf3
cristobal-rebolledo <cfrebolledo1606@gmail.com>
2024-10-22 16:35:18 -0300

    Actualzación de interfaz de Camera en el formulario

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -1,20 +1,12 @@
 import { Result, Err, Ok } from '../commonStructures/resultEnum'
 import React, { useState } from 'react'
-import { Button, Image, View, StyleSheet, Alert, Linking, TouchableOpacity, Modal } from 'react-native'
+import { Button, Image, View, StyleSheet, Alert, Linking, TouchableOpacity, Modal, Text } from 'react-native'
 import * as ImagePicker from 'expo-image-picker'
 import * as MediaLibrary from 'expo-media-library'
-import { Text } from '@ui-kitten/components'
-/**
- * Class representing the configuration settings for the camera.
- */
+import cameraIcon from '../assets/camera.png'
+import galleryIcon from '../assets/gallery.png'
+
 class CameraConfiguration {
-  /**
-   * Create a CameraConfiguration instance.
-   * @param {function} setImage - Function to set the image.
-   * @param {boolean} [allowsEditing=true] - Indicates if editing is allowed.
-   * @param {Array<number>} [aspect=[4, 3]] - The aspect ratio for the camera.
-   * @param {number} [quality=1] - The quality of the image (0 to 1).
-   */
   constructor(setImage = img => {}, allowsEditing = true, aspect = [4, 3], quality = 1) {
     this.setImage = setImage
     this.allowsEditing = allowsEditing
@@ -22,13 +14,6 @@ class CameraConfiguration {
     this.quality = quality
   }
 
-  /**
-   * Get the camera configuration settings.
-   * @returns {Object} The camera settings.
-   * @property {boolean} allowsEditing - Indicates if editing is allowed.
-   * @property {Array<number>} aspect - The aspect ratio for the camera.
-   * @property {number} quality - The quality of the image.
-   */
   getSettings() {
     return {
       allowsEditing: this.allowsEditing,
@@ -38,42 +23,30 @@ class CameraConfiguration {
   }
 }
 
-
-/**
- * Camera component for capturing images or selecting them from the gallery.
- *
- * @param {Object} cameraConfiguration - Configuration object for the camera.
- * @param {function} cameraConfiguration.setImage - Function to set the selected image.
- * @param {function} cameraConfiguration.setImageSetter - Function to update the image setter function.
- * 
- * @returns {JSX.Element} Rendered camera component.
- */
 export const Camera = ({title, required, cameraConfiguration }) => {
-  const [image, setImage] = useState(null) //! Modify this when the display modal is no longer needed
-  const [isModalVisible, setModalVisible] = useState(false) 
+  const [image, setImage] = useState(null)
+  const [isMenuVisible, setMenuVisible] = useState(false)
 
   async function openMedia(cameraConfiguration, launchFunction) {
     let { status } = await ImagePicker.requestMediaLibraryPermissionsAsync()
-    
-    // Check if permissions are granted, if not show alert and return
+
     if (status !== 'granted') {
       Alert.alert(
         'Permissions Required',
         'Camera/Gallery permissions are required to proceed. Please enable them in your app settings.',
         [
-          { text: 'Cancel', style: 'cancel' }, 
-          { text: 'Open Settings', onPress: () => Linking.openSettings() } // Redirect to settings
+          { text: 'Cancel', style: 'cancel' },
+          { text: 'Open Settings', onPress: () => Linking.openSettings() }
         ]
       )
     }
     if (status !== 'granted') return (new Err('Permissions not granted by user')).show()
-  
+
     const result = await launchFunction(cameraConfiguration.getSettings())
     if (result.canceled) return new Err('Operation cancelled')
-    
+
     const imageUri = result.assets[0].uri
-  
-    // Save the image in the gallery
+
     if (launchFunction === ImagePicker.launchCameraAsync) {
       try {
         const asset = await MediaLibrary.createAssetAsync(imageUri)
@@ -82,46 +55,71 @@ export const Camera = ({title, required, cameraConfiguration }) => {
         return new Err(`Error saving file in gallery: ${error}`).show()
       }
     }
-    
-    console.log(cameraConfiguration)
-    cameraConfiguration.setImage(imageUri) // Save the image in the state
+
+    cameraConfiguration.setImage(imageUri)
+    setImage(imageUri) // Actualiza la imagen seleccionada
     return new Ok(imageUri)
   }
 
   const openCamera = async () => openMedia(cameraConfiguration, ImagePicker.launchCameraAsync)
   const openGallery = async () => openMedia(cameraConfiguration, ImagePicker.launchImageLibraryAsync)
-  const toggleModal = () => setModalVisible(!isModalVisible) 
-  
-  // Create a new function that combines the existing behavior with additional functionality
-  const originalSetImage = cameraConfiguration.setImage.bind(cameraConfiguration) // Preserve the original context
-  
-  //! Modify this when the display modal is no longer needed
+  const toggleMenu = () => setMenuVisible(!isMenuVisible)
+
+  const originalSetImage = cameraConfiguration.setImage.bind(cameraConfiguration)
+
   cameraConfiguration.setImage = img => {
-      originalSetImage(img) // Call the original setImage function
-      setImage(img) // Call the additional function
-  } 
+    originalSetImage(img)
+    setImage(img)
+  }
+
+  const removeImage = () => {
+    setImage(null)
+    cameraConfiguration.setImage(null)
+    toggleMenu()
+  }
 
   return (
     <View style={styles.container}>
       {title && (
-        <Text category="h6">
+        <Text style={styles.title}>
           {title}
           {required ? "*" : ""}
         </Text>
       )}
-      <Button title="Abrir Cámara" onPress={openCamera} />
-      <Button title="Seleccionar Imagen de Galería" onPress={openGallery} />
+      
+      {/* Contenedor que muestra el icono o imagen seleccionada */}
+      <TouchableOpacity onPress={toggleMenu} style={styles.imageContainer} activeOpacity={0.7}>
+        {image ? (
+          <Image source={{ uri: image }} style={styles.selectedImage} />
+        ) : (
+          <Image source={cameraIcon} style={styles.cameraIcon} />
+        )}
+      </TouchableOpacity>
 
-      {image && (
-        <TouchableOpacity onPress={toggleModal}>
-          <Image source={{ uri: image }} style={styles.thumbnail} />
-        </TouchableOpacity>
-      )}
-
-      <Modal visible={isModalVisible} transparent={true}>
+      {/* Menú emergente (modal) con las opciones de Cámara, Galería y Eliminar Imagen */}
+      <Modal visible={isMenuVisible} transparent={true} animationType="fade">
         <View style={styles.modalContainer}>
-          <Image source={{ uri: image }} style={styles.fullImage} />
-          <Button title="Cerrar" onPress={toggleModal} />
+          <View style={styles.menu}>
+          <Text style={styles.title}>Seleccione una opción</Text>
+            <TouchableOpacity onPress={openCamera} style={styles.menuItem}>
+              <Image source={cameraIcon} style={styles.icon} />
+              <Text style={styles.menuText}>Cámara</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={openGallery} style={styles.menuItem}>
+              <Image source={galleryIcon} style={styles.icon} />
+              <Text style={styles.menuText}>Galería</Text>
+            </TouchableOpacity>
+
+            {/* Mostrar el botón de eliminar sólo si hay una imagen */}
+            {image && (
+              <TouchableOpacity onPress={removeImage} style={styles.menuItem}>
+                <Text style={{...styles.menuText, color: "red"}}>Eliminar Imagen</Text>
+              </TouchableOpacity>
+            )}
+
+            <Button title="Cerrar" onPress={toggleMenu} />
+          </View>
         </View>
       </Modal>
     </View>
@@ -131,25 +129,65 @@ export const Camera = ({title, required, cameraConfiguration }) => {
 export default CameraConfiguration
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+  title: {
+    fontSize: 20, // Tamaño de fuente
+    fontWeight: 'bold', // Texto en negrita
+    marginBottom: 5, // Espacio debajo del título
+    textAlign: 'left', // Alinear texto a la izquierda
+    alignSelf: 'flex-start' // Alinear el componente a la izquierda dentro de su contenedor
   },
-  thumbnail: {
-    width: 100,
-    height: 100,
-    marginTop: 20,
+  container: {
+    marginVertical: 10,
+    alignItems: 'center' // Centra el texto y el ícono en la misma línea vertical
+  },
+  imageContainer: {
+    width: 300, // Ajusta el tamaño del contenedor
+    height: 150,
+    backgroundColor: '#f0f0f0', // Fondo gris claro
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10, // Bordes redondeados
+    marginBottom: 20, // Espacio inferior
+    shadowColor: '#000', // Sombra para dar efecto de botón
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5 // Solo para Android
+  },
+  cameraIcon: {
+    width: 80, // Tamaño del ícono de cámara
+    height: 80
+  },
+  selectedImage: {
+    width: '100%', // La imagen seleccionada ocupa todo el contenedor
+    height: '100%',
     borderRadius: 10
   },
   modalContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.8)'
+    backgroundColor: 'rgba(0, 0, 0, 0.5)' // Fondo transparente oscuro
   },
-  fullImage: {
-    width: 300,
-    height: 300
+  menu: {
+    backgroundColor: 'white', // Fondo blanco del menú
+    borderRadius: 10,
+    padding: 20,
+    alignItems: 'center',
+    width: 300, // Ajusta el tamaño del contenedor
+    height: 260
+  },
+  menuItem: {
+    flexDirection: 'row', // Ícono y texto en línea
+    alignItems: 'center',
+    marginVertical: 10
+  },
+  icon: {
+    width: 40, // Ajusta el tamaño de los íconos del menú
+    height: 40,
+    marginRight: 10
+  },
+  menuText: {
+    fontSize: 18
   }
 })
