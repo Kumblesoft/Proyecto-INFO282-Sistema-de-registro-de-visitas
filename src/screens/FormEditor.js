366ddf9d4f9e601350b7eb0c3f5b7060d9716391
BenrriXWorks <BenrriXWorks@gmail.com>
2024-12-09 22:05:13 -0300

    Nombre formulario unico, FieldS..List usa Map

--- src/screens/FormEditor.js
+++ src/screens/FormEditor.js
@@ -1,17 +1,17 @@
 import React, { useState } from "react"
 import { ScrollView, View, StyleSheet } from "react-native"
-import { Text, Input, Button, Layout } from "@ui-kitten/components"
+import { Text, Input, Layout, Icon, Button } from "@ui-kitten/components"
 import formTemplate from "../fieldsConstructor/fields.json" // Importar el JSON con los campos
 import FieldSelector from "../components/FieldSelector" 
 
 export default function FormEditor() {
   const [formFields, setFormFields] = useState(formTemplate)
   const [selectedField, setSelectedField] = useState("") // Estado para el FieldSelector
-  const createdFields = []
+  const createdFields = [] 
+  const formNames = new Set(require("../TestForms/forms.json").map(form => form["nombre formulario"]))
+  const [isNameTaken, setIsNameTaken] = useState(false)
 
-  const handleFieldPos = (field) => {
-    createdFields.push(field)
-  }
+  const handleFieldPos = createdFields.push
 
   const handleFieldChange = (fieldKey, fieldProperty, value) => {
     setFormFields(prevFields => ({
@@ -23,6 +23,12 @@ export default function FormEditor() {
     }))
   }
 
+  const checkFormName = name => {
+    console.log(name)
+    console.log(formNames)
+    setIsNameTaken(formNames.has(name))
+    console.log(formNames.has(name))
+  }
   const renderField = (fieldKey, field) => {
     return (
       <Layout key={fieldKey} style={styles.fieldContainer}>
@@ -31,7 +37,7 @@ export default function FormEditor() {
           <Input
             style={styles.input}
             value={field.valor}
-            OnChange={(text) => handleFieldChange(fieldKey, "valor", text)} // Cambiar el valor
+            onChangeText={text => handleFieldChange(fieldKey, "valor", text)} // Cambiar el valor
             placeholder={`Ingrese ${field.nombre}`}
           />
         )}
@@ -52,17 +58,24 @@ export default function FormEditor() {
   return (
     <ScrollView style={styles.container}>
       <Text style={styles.label}>Nombre del Formulario</Text>
-      <Input placeholder="Nombre del Formulario" style={styles.input} />
+      <Input placeholder="Nombre del Formulario" style={styles.input} textStyle={{ color: isNameTaken ? 'red' : 'black' }} onChangeText={checkFormName}/>
+      {isNameTaken && 
+        <Layout size='small' style={styles.alert}>
+          <Icon status='danger' fill='#FF0000' name='alert-circle' style={styles.icon} />
+          <Text style={{ color: 'red' }}>Este nombre de plantilla ya existe</Text>
+        </Layout>
+      }
       {/* Campo Selector */}
       <FieldSelector
         selectedValue={selectedField}
-        onValueChange={(value) => setSelectedField(value)} // Actualiza el estado cuando cambia el selector
+        onValueChange={setSelectedField} // Actualiza el estado cuando cambia el selector
       />
 
       {/* Renderizar campos del formulario */}
       {selectedField && renderField(selectedField, formFields[selectedField])}
 
-      {/* Botón para guardar cambios */}
+      {/* Botón para guardar cambios <Button onPress={handleFieldPos} margin='20' padding='20' title="Guardar" /> */}
+      
     </ScrollView>
   )
 }
@@ -82,8 +95,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: "#ccc",
     padding: 10,
-    borderRadius: 5,
-    marginBottom: 10,
+    borderRadius: 5
   },
   optionContainer: {
     marginBottom: 10,
@@ -92,5 +104,20 @@ const styles = StyleSheet.create({
     fontSize: 16,
     marginVertical: 10,
     fontWeight: "bold",
+  },
+  icon: {
+    width: 20,
+    height: 20,
+  },
+  alert: {
+    backgroundColor: 'transparent',
+    flex: 1,
+    margin: 1,
+    marginHorizontal: '3%',
+    color: '#dd0000',
+    top: -10,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'left',
   },
 })
