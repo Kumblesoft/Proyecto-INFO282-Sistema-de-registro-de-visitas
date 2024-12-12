81d15ab519ce354c06833b62274a1f038e1cfaca
BladerKholin <bladerop.27@gmail.com>
2024-12-12 04:09:11 -0300

    Chains

--- src/database/componentInsertor/DateChainInsertor.js
+++ src/database/componentInsertor/DateChainInsertor.js
@@ -5,5 +5,10 @@ export default class DateChainInsertor extends ChainInsertor {
         if (fieldObject.type != 'fecha')
             return next && next.insert(fieldObject)
 
+        this.db.runSync(
+            `INSERT INTO ${fieldTableName} (fk_field, name, default_date) VALUES (?,?,?)`,
+            [fieldId, fieldObject.nombre, fieldObject.default_date]
+        )
+        // Colocar formato.
     }
 }
--- src/database/componentInsertor/SelectorChainInsertor.js
+++ src/database/componentInsertor/SelectorChainInsertor.js
@@ -1,9 +1,13 @@
 import ChainInsertor from './ChainInsertor'
 
+
 export default class SelectorChainInsertor extends ChainInsertor {
     insert(fieldObject, fieldId, fieldTypeId, fieldTableName) {
         if (fieldObject.type != 'selector')
             return next && next.insert(fieldObject)
-
+        this.db.runSync(
+            `INSERT INTO ${fieldTableName} (fk_field, name, default_option, selector_placeholder) VALUES (?,?,?)`,
+            [fieldId, fieldObject.nombre, fieldObject.default_option, selector_placeholder]
+        )
     }
 }
